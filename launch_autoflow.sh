60bd65e62260b06f96e3dfb801d14e1cd1cb2924
JoseCorCab <josecordoba@uma.es>
2021-05-19 16:34:34 +0200

    Options corrected and aux_scripts folders added

--- launch_autoflow.sh
+++ launch_autoflow.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 source ~soft_bio_267/initializes/init_autoflow
-
 CODE_PATH=`pwd`
+if [ ! -d $CODE_PATH/cohorts ]; then
+	$CODE_PATH/link_cohorts.sh
+fi
+
 output_path=$SCRATCH/phenotypes/cohortAnalyzer_wf
 cohorts_path=$CODE_PATH"/cohorts"
 cohorts='decipher;pmm2_CDG;id_mca'
-custom_opt=$CODE_PATH'/custom_opt.txt'
+cohorts='pmm2_CDG;id_mca'
+custom_opt=$CODE_PATH'/custom_opt'
 
 AF_VARS=`echo -e "
 			\\$all_cohorts=$cohorts,
 			\\$custom_options=$custom_opt,
-			\\$HPO='/mnt/home/users/bio_267_uma/elenarojano/dev_gem/pets/external_data/hp.obo'
+			\\$HPO='/mnt/home/users/bio_267_uma/elenarojano/dev_gem/pets/external_data/hp.obo',
+			\\$cohorts_path=$cohorts_path
  " | tr -d [:space:]`
 
 
-AutoFlow -w $CODE_PATH/cohortAnalyzer.af -V $AF_VARS -o $output_path -c 2 -m 20gb -t '1-00:00:00' $1
+AutoFlow -e -w $CODE_PATH/cohortAnalyzer.af -V $AF_VARS -o $output_path -c 2 -m 5gb -t '03:00:00' $1
--- link_cohorts.sh
+++ link_cohorts.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-cohort_path="/mnt/home/users/bio_267_uma/elenarojano/projects/pets/CohortAnalyzer/cohorts/paper_cohorts"
+cohort_path="/mnt/home/users/bio_267_uma/elenarojano/projects/pets/CohortAnalyzer/cohorts/paper_cohorts/files_to_combine"
 mkdir cohorts
 cd cohorts
 
 
-ln -s $cohort_path/../newrelease_decipher_all.txt ./decipher.txt
+ln -s $cohort_path/decipher_cohort_translated.txt ./decipher.txt
 ln -s $cohort_path/pmm2_paco_format.txt ./pmm2_CDG.txt
-ln -s $cohort_path/gold_standard_humu22442_results_phens.txt ./id_mca.txt
+ln -s $cohort_path/hummu_congenital_full_dataset.txt ./id_mca.txt
 
 cd ..
